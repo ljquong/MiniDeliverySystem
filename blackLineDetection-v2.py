7e35f1ac00b7f780c6cd139145ace7a23de463c8
ljquong <132942951+ljquong@users.noreply.github.com>
2025-03-22 22:57:54 -0600

    Add files via upload

---
+++ blackLineDetection-v2.py
@@ -0,0 +1,67 @@
+from machine import Pin, PWM
+from time import sleep
+from hcsr04 import HCSR04 # Must have this library saved on Pico to work
+
+# Initialize sensor with trigger and echo pins
+sensor = HCSR04(trigger_pin=21, echo_pin=20)
+on_board_LED = machine.Pin("LED", machine.Pin.OUT)
+
+line_sen = Pin(16, Pin.IN)
+
+# === L298N Motor Driver ===
+# Motor A
+motor_a_in1 = Pin(6, Pin.OUT)
+motor_a_in2 = Pin(7, Pin.OUT)
+motor_a_en = PWM(Pin(8))
+motor_a_en.freq(1000)
+motor_a_correction = 1.0 # Adjust so both motors have same speed
+
+# Motor B
+motor_b_in3 = Pin(4, Pin.OUT)
+motor_b_in4 = Pin(3, Pin.OUT)
+motor_b_en = PWM(Pin(2))
+motor_b_en.freq(1000)
+motor_b_correction = 1.0 # Adjust so both motors have same speed
+
+# Function to control Motor A
+def motor_a(direction = "stop", speed = 0):
+    adjusted_speed = int(speed * motor_a_correction)  # Apply correction
+    if direction == "forward":
+        motor_a_in1.value(0)
+        motor_a_in2.value(1)
+    elif direction == "backward":
+        motor_a_in1.value(1)
+        motor_a_in2.value(0)
+    else:  # Stop
+        motor_a_in1.value(0)
+        motor_a_in2.value(0)
+    motor_a_en.duty_u16(int(adjusted_speed * 65535 / 100))  # Speed: 0-100%
+
+# Function to control Motor B
+def motor_b(direction = "stop", speed = 0):
+    adjusted_speed = int(speed * motor_b_correction)  # Apply correction
+    if direction == "forward":
+        motor_b_in3.value(1)
+        motor_b_in4.value(0)
+    elif direction == "backward":
+        motor_b_in3.value(0)
+        motor_b_in4.value(1)
+    else:  # Stop
+        motor_b_in3.value(0)
+        motor_b_in4.value(0)
+    motor_b_en.duty_u16(int(adjusted_speed * 65535 / 100))  # Speed: 0-100%
+
+
+# Example
+#put this in a function
+while True:
+    motor_a("forward", 35)
+    motor_b("backward", 35)
+    sleep(0.1)
+    if line_sen.value() == 0:
+        motor_a()
+        motor_b()
+        break
+
+# moves approx 13cm at speed 35 for 0.5 seconds
+# 90 degree turn at speed 35 for 0.39
