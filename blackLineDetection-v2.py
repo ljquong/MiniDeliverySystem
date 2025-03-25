e8656b52370cc362a4a2c413838b3303c0acf371
ljquong <132942951+ljquong@users.noreply.github.com>
2025-03-24 23:44:30 -0600

    Update blackLineDetection-v2.py

--- blackLineDetection-v2.py
+++ blackLineDetection-v2.py
@@ -1,10 +1,10 @@
 from machine import Pin, PWM
 from time import sleep
-from hcsr04 import HCSR04
+from hcsr04 import HCSR04 
 
 # Initialize sensor with trigger and echo pins
 sensor = HCSR04(trigger_pin=21, echo_pin=20)
-on_board_LED = machine.Pin("LED", machine.Pin.OUT)
+led = machine.Pin("LED", machine.Pin.OUT)
 
 line_sen = Pin(16, Pin.IN)
 
@@ -50,15 +50,23 @@ def motor_b(direction = "stop", speed = 0):
         motor_b_in3.value(0)
         motor_b_in4.value(0)
     motor_b_en.duty_u16(int(adjusted_speed * 65535 / 100))  # Speed: 0-100%
-
+    
 while True:
-    motor_a("forward", 35)
-    motor_b("forward", 35)
+    motor_a("forward", 31)
+    motor_b("forward", 31)
     sleep(0.1)
     if line_sen.value() == 0:
         motor_a()
         motor_b()
         break
+    
+sleep(3)
+motor_a("forward", 35)
+motor_b("backward", 35)
+sleep(0.39)
+
+motor_a()
+motor_b()
 
 # moves approx 13cm at speed 35 for 0.5 seconds
 # 90 degree turn at speed 35 for 0.39
