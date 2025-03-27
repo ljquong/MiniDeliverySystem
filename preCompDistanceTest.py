4f686e0065e44ae9b9299a8eef47a810760b28d8
ljquong <132942951+ljquong@users.noreply.github.com>
2025-03-27 16:46:41 -0600

    Add files via upload
    Motor test

---
+++ preCompDistanceTest.py
@@ -0,0 +1,46 @@
+from machine import Pin, PWM
+from time import sleep
+
+motor_a_in1 = Pin(6, Pin.OUT)
+motor_a_in2 = Pin(7, Pin.OUT)
+motor_a_en = PWM(Pin(8))
+motor_a_en.freq(1000)
+motor_a_correction = 1.0
+
+motor_b_in3 = Pin(4, Pin.OUT)
+motor_b_in4 = Pin(3, Pin.OUT)
+motor_b_en = PWM(Pin(2))
+motor_b_en.freq(1000)
+motor_b_correction = 1.0
+
+def motor_a(direction = "stop", speed = 0):
+    adjusted_speed = int(speed * motor_a_correction)
+    if direction == "forward":
+        motor_a_in1.value(0)
+        motor_a_in2.value(1)
+    elif direction == "backward":
+        motor_a_in1.value(1)
+        motor_a_in2.value(0)
+    else:
+        motor_a_in1.value(0)
+        motor_a_in2.value(0)
+    motor_a_en.duty_u16(int(adjusted_speed * 65535 / 100))
+
+def motor_b(direction = "stop", speed = 0):
+    adjusted_speed = int(speed * motor_b_correction)
+    if direction == "forward":
+        motor_b_in3.value(1)
+        motor_b_in4.value(0)
+    elif direction == "backward":
+        motor_b_in3.value(0)
+        motor_b_in4.value(1)
+    else:
+        motor_b_in3.value(0)
+        motor_b_in4.value(0)
+    motor_b_en.duty_u16(int(adjusted_speed * 65535 / 100))
+
+motor_a("forward", 35)
+motor_b("forward", 35)
+sleep(10)
+motor_a()
+motor_b()
