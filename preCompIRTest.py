b4cbfab604a5bbef411596e9d964df9faacf6dc0
ljquong <132942951+ljquong@users.noreply.github.com>
2025-03-28 14:00:27 -0600

    Add files via upload

---
+++ preCompIRTest.py
@@ -0,0 +1,12 @@
+from machine import Pin, ADC
+from time import sleep
+
+ir = ADC(28)
+led = Pin('LED', Pin.OUT)
+
+while True:
+    if ir.read_u16() > 2300: # abitrary value, must test output values
+        led.on()
+    else:
+        led.off()
+    sleep(0.1)
