658cc4e041eb6aa34c316335ab2e2eed413a5f0c
ljquong <132942951+ljquong@users.noreply.github.com>
2025-04-02 14:50:46 -0600

    Update preCompDistanceTest.py
    Updated motor driver with accurate course correction values and tested time delay for distance

--- preCompDistanceTest.py
+++ preCompDistanceTest.py
@@ -5,7 +5,7 @@ motor_a_in1 = Pin(14, Pin.OUT)
 motor_a_in2 = Pin(13, Pin.OUT)
 motor_a_en = PWM(Pin(15))
 motor_a_en.freq(1000)
-motor_a_correction = 1.0
+motor_a_correction = 1.03
 
 motor_b_in3 = Pin(4, Pin.OUT)
 motor_b_in4 = Pin(3, Pin.OUT)
@@ -40,8 +40,8 @@ def motor_b(direction = "stop", speed = 0):
     motor_b_en.duty_u16(int(adjusted_speed * 65535 / 100))
 
 motor_a("forward", 35)
-motor_b("forward", 35)
-sleep(10)
+motor_b("backward", 35)
+sleep(6)
 motor_a()
 motor_b()
 sleep(60)
